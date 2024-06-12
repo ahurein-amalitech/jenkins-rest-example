7ade65255b72f530562569d62101dc98f58b58de
ahurein-amalitech <ebenezer.ahurein@amalitech.com>
2024-06-12 10:38:19 -0400

    initial file

---
+++ src/main/java/com/example/jenkinsrestexample/JenkinsRestExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsrestexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsRestExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsRestExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkinsrestexample/JenkinsRestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsrestexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsRestExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
